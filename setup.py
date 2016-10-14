4cf39843d151440a3260852334039aebf8974b94
vanonselenp <vanonselen.p@gmail.com>
2016-10-14 10:23:00 +0200

    added a setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='engarde',
+    version='0.0.1',
+    url='https://github.com/vanonselenp/engarde',
+    description='Random remake reskin of existing website',
+    install_requires=[
+        'markdown',
+        'flask',
+    ],
+)
